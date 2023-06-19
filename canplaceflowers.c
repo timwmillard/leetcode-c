0ea29fa71ec1caca25d7fa962fdb04944222d5ef
Tim Millard <tim@rapidpaylegal.com>
2023-06-19 22:39:06 +1000

    Solved Can Place Flowers - Created table driven test

--- canplaceflowers.c
+++ canplaceflowers.c
@@ -7,37 +7,73 @@ bool canPlaceFlowers(int *flowerbed, int flowerbedSize, int n) {
 
     for (int i = 0; i < flowerbedSize; i++) {
         bool plot = flowerbed[i];
-        bool left = true;
-        bool right = true;
+        bool left = false;
+        bool right = false;
         if (i > 0)
             left = flowerbed[i-1];
         if (i < flowerbedSize - 1)
             right = flowerbed[i+1];
 
         bool canPlant = !plot && !left && !right;
-        if (canPlant)
+        if (canPlant) {
+            flowerbed[i] = true;
             n--;
-
+        }
     }
     return n <= 0;
 }
 
 int main() {
 
-    int flowerbed1[] = {1, 0, 0, 0, 1};
-    bool got1 = canPlaceFlowers(flowerbed1, 5, 1);
-    printf("got1 = %s\n", got1? "true" : "false");
-    assert(got1 == true);
+    struct test {
+        char *name;
+        int *flowerbed;
+        int flowerbedSize;
+        int n;
+        bool output;
+    };
 
-    int flowerbed2[] = {1, 0, 0, 0, 1};
-    bool got2 = canPlaceFlowers(flowerbed2, 5, 2);
-    printf("got2 = %s\n", got2? "true" : "false");
-    assert(got2 == false);
+    struct test tests[] = {
+        {
+            .name = "Example 1",
+            .flowerbed = (int[]){1, 0, 0, 0, 1},
+            .flowerbedSize = 5,
+            .n = 1,
+            .output = true
+        },
+        {
+            .name = "Example 2",
+            .flowerbed = (int[]){1, 0, 0, 0, 1},
+            .flowerbedSize = 5,
+            .n = 2,
+            .output = false
+        },
+        {
+            .name = "Example 3",
+            .flowerbed = (int[]){1, 0, 0, 0, 0, 1},
+            .flowerbedSize = 6,
+            .n = 2,
+            .output = false 
+        },
+        {
+            .name = "Example 4",
+            .flowerbed = (int[]){0, 0, 1, 0, 1},
+            .flowerbedSize = 5,
+            .n = 1,
+            .output = true
+        }
+    };
 
-    int flowerbed3[] = {1, 0, 0, 0, 0, 1};
-    bool got3 = canPlaceFlowers(flowerbed3, 6, 2);
-    printf("got3 = %s\n", got3? "true" : "false");
-    assert(got3 == false);
+    for (int i = 0; i < 4; i++) {
+        struct test t = tests[i];
+
+        bool got1 = canPlaceFlowers(t.flowerbed, t.flowerbedSize, t.n);
+        printf("%s = %s\n", t.name, got1? "true" : "false");
+        assert(got1 == t.output);
+    }
+
+
+    printf("\n***** All test passed ******\n");
 
     return 0;
 }
